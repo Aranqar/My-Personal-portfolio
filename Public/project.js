ff332586d37e301ef1e28494c9249abcf76414d0
Aranqar <Aranqar@ya.ru>
2021-10-18 14:35:30 +0300

    Preliminary styles and pages

---
+++ Public/project.js
@@ -0,0 +1,12 @@
+let projects = [
+{
+    name: 'project one',
+    tags: '#javascript',
+    image: 'project (1).png'
+},
+{
+    name: 'project two',
+    tags: '#javascript',
+    image: 'project (2).png'
+}
+]
